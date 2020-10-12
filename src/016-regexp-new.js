096b349f607a67d9db50c475e821a295f0d30fd4
cseve123 <cseve123@163.com>
2020-10-12 22:58:12 +0800

    regexp s ?<> ?<=

--- src/016-regexp-new.js
+++ src/016-regexp-new.js
@@ -13,7 +13,7 @@ console.log(reg2.exec(str));
 // u修饰符 unicode
 const str2 = '\uD842\uDFB7'; // 表示一个字符
 console.log(/^\uD842/.test(str2)) // es5 true
-console.log(/^\uD842/u.text(str2)) //es6 false
+// console.log(/^\uD842/u.text(str2)) //es6 false
 
 // .除了换行符以外的任意单个字符
 console.log(/^.$/.test(str2)) // false
@@ -21,3 +21,35 @@ console.log(/^.$/u.test(str2)) // true
 
 console.log(/\u{61}/.test('a')) // false
 console.log(/\u{61}/u.test('a')) // true
+
+// ---------------------------
+// ES9
+// dot点 www.imo.o
+// .配饰除换行的任意
+const reg3 = /./
+console.log(reg3.test('5')) // true
+console.log(reg3.test('\n')) // false
+console.log(reg3.test('\r')) // false
+console.log(reg3.test('\u{2028}')) // false
+
+// dotAll--新的修饰符s,真正的匹配任意
+const reg4 = /./s
+console.log(reg4.test('5')) // true
+console.log(reg4.test('\n')) // false
+console.log(reg4.test('\r')) // false
+console.log(reg4.test('\u{2028}')) // false
+// 修饰符：g i m y u s
+
+// 具名组匹配
+// const date = /(\d{4})-(\d{2}-(\d{2}))/.exec('2020-10-12');
+// console.log(date[1], date[2], date[3]);
+const date = /(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})/.exec('2020-10-12');
+console.log(date);
+const {year, month, day} = date.groups;
+console.log(year,month,day);
+
+// 先行断言
+const str9 = 'ecmascript';
+console.log(str9.match(/ecma(?=script)/));
+// 后行断言
+console.log(str9.match(/(?<=ecms)script/));
--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@
 // import './013-set'
 // import './014-map';
 // import './015-string-new';
-// import './016-regexp-new';
+import './016-regexp-new';
 // import './017-number-new'
 // import './018-proxy'
 // import './019-reflect'
 // import './021-promise'
 // import './022-generator'
-import './023-iterator'
+// import './023-iterator'
 // import './024-module'
 // import './025-async-await';
 // let sum = (x, y) => x + y
